86803b5d4ad3bb2acac51720de8702fbf5173737
lusmacaron <ehdtjd366026@gmail.com>
2025-08-18 12:07:09 +0900

    feat: 설정페이지에 도움말 추가

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -8,14 +8,13 @@ class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( // 설정 페이지 자체에 AppBar를 추가합니다.
+      appBar: AppBar(
         title: const Text('설정'),
-        automaticallyImplyLeading: false, // BottomNavigationBar로 접근되므로 뒤로가기 버튼 숨김
+        automaticallyImplyLeading: false,
       ),
-      body: ListView( // 설정 항목이 많아지면 스크롤 가능하도록 ListView 사용
-        padding: const EdgeInsets.all(16.0), // 전체적인 페이지 패딩
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
         children: [
-          // 개인정보 설정
           ListTile(
             leading: Icon(Icons.person_outline),
             title: Text('개인정보 설정'),
@@ -26,7 +25,6 @@ class SettingPage extends StatelessWidget {
           ),
           Divider(),
 
-          // 알림 설정
           ListTile(
             leading: Icon(Icons.notifications_none), // 알림 관련 아이콘
             title: Text('알림 설정'),
@@ -37,7 +35,6 @@ class SettingPage extends StatelessWidget {
           ),
           Divider(),
 
-          // 기기 설정
           ListTile(
             leading: Icon(Icons.devices_other),
             title: Text('기기 설정'),
@@ -48,7 +45,6 @@ class SettingPage extends StatelessWidget {
           ),
           Divider(),
 
-          // 영상 설정
           ListTile(
             leading: Icon(Icons.videocam),
             title: Text('영상 설정'),
@@ -59,7 +55,6 @@ class SettingPage extends StatelessWidget {
           ),
           Divider(),
 
-          // 테마 설정
           ListTile(
             leading: Icon(Icons.brightness_6), // 밝기/테마 관련 아이콘
             title: Text('테마 설정'),
@@ -70,12 +65,21 @@ class SettingPage extends StatelessWidget {
           ),
           Divider(),
 
-          // 언어 설정
           ListTile(
             leading: Icon(Icons.language), // 언어 관련 아이콘
             title: Text('언어 설정'),
             onTap: () {
               print('언어 설정 메뉴 클릭 -> 언어 설정 상세 페이지로 이동');
+              // TODO: 언어 설정 상세 페이지로 이동
+            },
+          ),
+          Divider(),
+
+          ListTile(
+            leading: Icon(Icons.help_outline), // 언어 관련 아이콘
+            title: Text('도움말'),
+            onTap: () {
+              print('도움말 메뉴 클릭 -> 도움말 상세 페이지 이동');
               // TODO: 언어 설정 상세 페이지로 이동
             },
           ),
