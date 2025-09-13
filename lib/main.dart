30b45b0e5293ab8dd67fd8aeda83a90365f819f7
lusmacaron <ehdtjd366026@gmail.com>
2025-09-13 16:16:59 +0900

    design: 테마수정

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Wildfire watcher',
       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
         primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          // AppBar의 기본 테마를 설정할 수 있습니다.
+          backgroundColor: Colors.white, // AppBar의 기본 배경색 (AppBar 자체의 색상)
+          foregroundColor: Colors.black, // AppBar 아이콘 및 텍스트 색상
+          elevation: 0, // AppBar 기본 그림자 제거
+        ),
       ),
       initialRoute: '/',
       routes: appRoutes, // 라우트 맵 연결
